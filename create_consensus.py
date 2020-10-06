df36c028f49788fdba8b8f562bf628d3f84f4670
jfnavarro <jc.fernandez.navarro@gmail.com>
2020-10-06 16:41:37 +0200

    Added more filters

--- create_consensus.py
+++ create_consensus.py
@@ -31,7 +31,7 @@ def consensus_maker(grouped_reads_list, cut_off):
     num_reads = float(len(grouped_reads_list))
     counters = [Counter([x[i] for x in grouped_reads_list]) for i in range(read_length)]
     consensus_read = ''.join(
-        [c.most_common()[0][0] if (c.most_common()[0][1] / num_reads) > cut_off else 'N' for c in counters])
+        [c.most_common()[0][0] if (c.most_common()[0][1] / num_reads) >= cut_off else 'N' for c in counters])
     return consensus_read
 
 def consensus_quality(qual_list):
@@ -44,13 +44,13 @@ def main():
     parser = ArgumentParser()
     parser.add_argument('infile', type=str, help="input BAM file")
     parser.add_argument("--outfile", action="store", dest="outfile", help="output BAM file [out.bam]",
-                        default="out.bam")
-    parser.add_argument('--min-reads', type=int, default=3, dest='min_reads',
-                        help="Minimum number of reads allowed to comprise a consensus. [3]")
+                        default="out_duplex.bam")
+    parser.add_argument('--min-reads', type=int, default=2, dest='min_reads',
+                        help="Minimum number of reads allowed to comprise a consensus. [2]")
     parser.add_argument('--max-reads', type=int, default=1000, dest='max_reads',
                         help="Maximum number of reads allowed to comprise a consensus. [1000]")
-    parser.add_argument('--homo-count', type=int, default=6, dest='rep_filt',
-                        help="Maximum number of homopolymer supported in the tag [A, C, G, T]. [6]")
+    parser.add_argument('--homo-count', type=int, default=5, dest='rep_filt',
+                        help="Maximum number of homopolymer supported in the tag [A, C, G, T]. [5]")
     parser.add_argument('--cutoff', type=float, default=.7, dest='cut_off',
                         help="Percentage of nucleotides at a given position in a read that must be identical in order "
                              "for a consensus to be called at that position. [0.7]")
@@ -60,6 +60,8 @@ def main():
                         help="Discard reads overlapping with their pair-end mate.")
     parser.add_argument('--filter-pair', action="store_true", default=False, dest='pair_filter',
                         help="Discard reads that do not have a proper pair-end mate.")
+    parser.add_argument('--filter-secondary', action="store_true", default=False, dest='secondary_filter',
+                        help="Discard reads that are not primary alignment.")
     parser.add_argument('--filter-singleton', action="store_true", default=False, dest='single_filter',
                         help="Discard reads that are singletons (only one pair is aligned).")
     parser.add_argument('--maxN', type=float, default=.5, dest='Ncut_off',
@@ -69,7 +71,6 @@ def main():
     # Variables
     reads_count = 0
     reads_count_filtered = 0
-    discarded_count = 0
     consensus_count = 0
     duplex_count = 0
     read_dict = defaultdict(lambda: defaultdict(list))
@@ -80,20 +81,18 @@ def main():
     Ncut_off = o.Ncut_off
     do_overlap_filter = o.softclip_filter
     do_softclip_filter = o.overlap_filter
+    do_secondary = o.secondary_filter
     do_pair_filter = o.pair_filter
     do_single_filter = o.single_filter
-    do_N_filter = Ncut_off < 1.0 and Ncut_off > 0.0
+    do_N_filter = Ncut_off <= 1.0 and Ncut_off >= 0.0
 
     # Start going through the input BAM file, one position at a time to group reads by start_pos + tag
     in_bam_file = pysam.Samfile(o.infile, "rb")
     discarded_bam_file = pysam.Samfile('discarded.bam', "wb", template=in_bam_file)
     out_bam_file = pysam.Samfile(o.outfile, "wb", template=in_bam_file)
+    out_bam_file_simplex = pysam.Samfile('out_simplex.bam', "wb", template=in_bam_file)
     print("Grouping reads by position-tag...")
     for record in in_bam_file.fetch(until_eof=True):
-        #  Only consider mapped reads
-        if record.is_unmapped:
-            continue
-
         reads_count += 1
 
         #  Get tag
@@ -101,12 +100,14 @@ def main():
         tag += ":1" if record.is_read1 else ":2"
 
         # Mapping filters
+        unmapped = record.is_unmapped
         singleton = do_single_filter and record.mate_is_unmapped
-        proper_pair = do_pair_filter and not record.is_proper_pair
+        not_proper_pair = do_pair_filter and not record.is_proper_pair
+        secondary = do_secondary and record.is_secondary
 
         # Overlap filter
         overlap = False
-        if do_overlap_filter and proper_pair and not singleton:
+        if do_overlap_filter and not not_proper_pair and not singleton:
             overlap = (record.reference_start <= record.next_reference_start < (
                     record.reference_start + record.query_length)) or \
                       (record.next_reference_start <= record.reference_start < (
@@ -127,8 +128,7 @@ def main():
         homo_T = tag.count('T') >= homo_count
 
         # Check if the given read is good
-        if any([overlap, soft_clip, homo_A, homo_C, homo_G, homo_T, singleton, proper_pair]):
-            discarded_count += 1
+        if any([overlap, soft_clip, homo_A, homo_C, homo_G, homo_T, singleton, not_proper_pair, unmapped, secondary]):
             discarded_bam_file.write(record)
         else:
             reads_count_filtered += 1
@@ -145,11 +145,9 @@ def main():
             sequences = [x.query_sequence for x in records]
             qualities = [x.query_qualities for x in records]
             num_sequences = len(sequences)
-            if num_sequences >= min_reads and num_sequences <= max_reads:
-                #  All sequences same length filter
-                first_length = len(sequences[0])
-                if not all(len(x) == first_length for x in sequences):
-                    continue
+            first_length = len(sequences[0])
+            if num_sequences >= min_reads and num_sequences <= max_reads and\
+                    all(len(x) == first_length for x in sequences):
                 consensus = consensus_maker(sequences, cut_off)
                 consensus_qual = consensus_quality(qualities)
                 # Filter out consensuses with too many Ns in them
@@ -183,6 +181,20 @@ def main():
             switch_tag = "{}{}:{}".format(clean_tag[int(len(clean_tag) / 2):],
                                           clean_tag[:int(len(clean_tag) / 2)],
                                           index)
+            # Create SAM record
+            a = pysam.AlignedRead()
+            a.query_name = tag
+            a.flag = record.flag
+            a.query_sequence = record.query_sequence
+            a.reference_id = record.reference_id
+            a.reference_start = record.reference_start
+            a.mapping_quality = 255
+            a.cigartuples = record.cigartuples
+            a.next_reference_id = record.next_reference_id
+            a.next_reference_start = record.next_reference_start
+            a.template_length = record.template_length
+            a.query_qualities = record.query_qualities
+            # Find duplex
             try:
                 simplex1 = record.query_sequence
                 simplex2 = consensus_dict[switch_tag].query_sequence
@@ -197,29 +209,17 @@ def main():
                 if do_N_filter and (duplex.count("N") / float(len(duplex)) >= Ncut_off):
                     continue
                 duplex_count += 1
+                a.query_sequence = duplex
+                out_bam_file.write(a)
             except KeyError:
-                duplex = record.query_sequence
-            # Create SAM record
-            a = pysam.AlignedRead()
-            a.query_name = tag
-            a.flag = record.flag
-            a.query_sequence = str(Seq(duplex).reverse_complement()) if a.is_reverse else duplex
-            a.reference_id = record.reference_id
-            a.reference_start = record.reference_start
-            a.mapping_quality = 255
-            a.cigartuples = record.cigartuples
-            a.next_reference_id = record.next_reference_id
-            a.next_reference_start = record.next_reference_start
-            a.template_length = record.template_length
-            a.query_qualities = record.query_qualities
-            #  Write SAM record
-            out_bam_file.write(a)
+                out_bam_file_simplex.write(a)
     out_bam_file.close()
+    out_bam_file_simplex.close()
 
     # Write summary statistics
     print("Summary:")
     print("Reads processed: {}".format(reads_count))
-    print("Reads discarded: {}".format(discarded_count))
+    print("Reads discarded: {}".format(reads_count - reads_count_filtered))
     print("Reads passing filters: {}".format(reads_count_filtered))
     print("Consensus Made: {}".format(consensus_count))
     print("Duplex Made: {}".format(duplex_count))
